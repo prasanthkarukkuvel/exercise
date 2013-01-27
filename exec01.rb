ed8a36a1699af197ab3785727b96ec140ee079c3
M.Shankari <shankari.msec@gmail.com>
2013-01-27 17:38:01 +0530

    Exercise 01

---
+++ exec01.rb
@@ -0,0 +1,11 @@
+puts "Enter a number between 1 and 100"
+n = gets.to_i
+until n > 1 && n < 100
+	puts "invalid number. pls enter again"
+	n = gets.to_i
+end
+n.times do |x|
+	if ( x % 2 ) != 0
+		puts x
+	end
+end
