881b219d268f80065e92f6a7301f504de7db4c50
Prasanth <prashanth702@gmail.com>
2013-01-28 09:17:33 +0530

    Execrise No 3 01

--- exec03.rb
+++ exec03.rb
@@ -4,7 +4,5 @@ if ARGV.length == 3
 	puts ARGV[2].split(",").select{|x| x.to_i >= ARGV[0].to_i && x.to_i <= ARGV[1].to_i && x.to_i%5 == 0}.inject{|i,j| i.to_i + j.to_i }
 	
 else
-
-	puts "Invalid number of arguments"	
-
+	puts ' Invalid number of arguments, specify the third params as String "" ex: "1,2,34,56"'	
 end
